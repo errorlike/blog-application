6f6f3767d167933e301e54dd910a0da59a8152ea
errorlike <errorlikeman@outlook.com>
2024-06-07 23:26:46 +0800

    exercise 13.5-13,7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,26 +4,31 @@ const blogFinder = async (request, response, next) => {
   request.blog = await Blog.findByPk(request.params.id);
   next();
 };
+
 router.get('/', async (request, response) => {
   const blogs = await Blog.findAll();
   response.json(blogs);
 });
+
 router.post('/', async (request, response) => {
-  try {
-    const blog = await (Blog.create(request.body));
-    response.status(201).json(blog);
-  } catch (error) {
-    response.status(400).json({ error });
+  const blog = await (Blog.create(request.body));
+  response.status(201).json(blog);
+});
+
+router.delete('/:id', blogFinder, async (request, response) => {
+  if (request.blog) {
+    await request.blog.destroy();
+    response.status(204).end();
   }
 });
-router.delete('/:id', blogFinder,async (request,  response) => {
-  try {
-    if (request.blog) {
-      await request.blog.destroy();
-      response.status(204).end();
-    }
-  } catch (error) {
-    response.status(404).json({ error });
+
+router.put('/:id', blogFinder, async (request, response) => {
+  if (request.blog) {
+    request.blog.likes = request.body.likes;
+    request.blog.save();
+    response.json(request.blog);
+  } else {
+    response.status(404).end();
   }
 });
 module.exports = router;
--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const { PORT } = require('./util/config');
+const { connectToDatabase } = require('./util/db');
+require('express-async-errors');
+const blogsRouter = require('./controllers/blogs');
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  console.error(error.name);
+  if (error.name === 'SequelizeValidationError') {
+    return response.status(400).send({ error: error.message });
+  }
+  if (error.name === 'SequelizeDatabaseError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
+};
 
-const { PORT } = require('./util/config')
-const { connectToDatabase } = require('./util/db')
-
-const blogsRouter = require('./controllers/blogs')
-
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
+app.use(express.json());
+app.use('/api/blogs', blogsRouter);
+app.use(errorHandler);
 
 const start = async () => {
-  await connectToDatabase()
+  await connectToDatabase();
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
-}
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-start()
+start();
