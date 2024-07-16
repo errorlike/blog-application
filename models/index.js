76d18c5e564f3a8241c951de009be05e874da970
errorlike <errorlikeman@outlook.com>
2024-07-16 23:27:34 +0800

    exercise 13.19

--- models/index.js
+++ models/index.js
@@ -1,8 +1,13 @@
 const Blog = require('./blog');
+const ReadingList = require('./reading_list');
 const User = require('./user');
 
 User.hasMany((Blog));
 Blog.belongsTo(User);
+
+User.belongsToMany(Blog, {through:ReadingList})
+Blog.belongsToMany(User, {through:ReadingList})
+
 module.exports = {
   Blog, User
 };
